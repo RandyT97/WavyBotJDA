8cd5ca96b3ab75f5b40d502620a4f678ba4f35aa
jawyuhz <rtsui9797@gmail.com>
2017-04-15 01:00:30 -0400

    Basic implementation of nbastreams link fetcher

--- src/main/java/Jawyuhz/DiscordBot/commands/NBACommand.java
+++ src/main/java/Jawyuhz/DiscordBot/commands/NBACommand.java
@@ -18,17 +18,13 @@ import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 public class NBACommand implements Command{
     private final String HELP = "Usage: " + Constants.PREFIX;
     private static UserAgent myUserAgent = UserAgent.of("desktop","jawyuhz.DiscordBot", "v0.1", "atomsapple1");
-    private static RedditClient redditClient = new RedditClient(myUserAgent);;
+    private static RedditClient redditClient = new RedditClient(myUserAgent);
     private static Credentials credentials = Credentials.script(Constants.REDDIT_USER,Constants.REDDIT_PASSWORD,Constants.REDDIT_CLIENT_ID,Constants.REDDIT_SECRET);
 
 
     public Boolean called(String[] args, MessageReceivedEvent event) {return true;}
 
     public void action(String[] args, MessageReceivedEvent event) {
-
-//        myUserAgent = UserAgent.of("desktop","jawyuhz.DiscordBot", "v0.1", "atomsapple1");
-//        redditClient = new RedditClient(myUserAgent);
-//        credentials = Credentials.script(Constants.REDDIT_USER,Constants.REDDIT_PASSWORD,Constants.REDDIT_CLIENT_ID,Constants.REDDIT_SECRET);
 
 
         if(args==null || args.length==0) {
@@ -46,45 +42,18 @@ public class NBACommand implements Command{
             for(Submission s: subreddit.next()) {
 
                 if(s.getTitle().toLowerCase().contains(args[0])) {
-                    System.out.println("FOUND");
                     Submission fullSubmissionInfo = redditClient.getSubmission(s.getId());
-                    System.out.println(s.getTitle());
-                    System.out.println(s.getId());
                     CommentNode node = fullSubmissionInfo.getComments();
-                    Comment comment = node.getComment();
-                    System.out.println(fullSubmissionInfo.getCommentCount());
-                    if(redditClient.getSubmission(s.getId()).getComments().getComment()!=null) {
-                        System.out.println(fullSubmissionInfo.getId());
-                        System.out.println(comment.getAuthor());
-                        System.out.println(comment.getBody());
+                    for(CommentNode n : node) {
+                        event.getTextChannel().sendMessage(n.getComment().getBody()).queue();
+
                     }
                 }
             }
-//            printComments(args[0], redditClient, "nbastreams", event);
-//                        SubredditPaginator link = new SubredditPaginator(redditClient , "wavesdontdie");
-//                        CommentStream commentPaginator = new CommentStream(redditClient,"wavesdontdie");
-//                        Listing<Submission> test = link.next();
-//                        System.out.println(test.size());
-//                        List<Comment> allComments
-////                        for(Submission sub : test) {
-////                            if(sub.getTitle().toLowerCase().contains(args[0])) {//Conditional Successful to find if team matches thread title
-//////                                System.out.println(commentPaginator.)
-////                                event.getTextChannel().sendMessage(sub.getTitle());
-////                                CommentNode comments = sub.getComments();
-////                                event.getTextChannel().sendMessage(comments.getComment().getBody()).queue();
-////
-////                            }
-////                            try {
-////                                Thread.sleep(1000);
-////                            }
-////                            catch(Exception e) {
-////
-////                            }
-////                        }
 
 
         } catch (OAuthException e) {
-            e.printStackTrace();
+            e.getStackTrace();
         }
 
 
