3d92420f82d08df2d3292f89e96842c62d2887d1
jawyuhz <rtsui9797@gmail.com>
2017-07-19 18:38:17 -0400

    Properly implemented Weather API Hashmap, Cleaned up code. TODO: Zipcode to lat/long and convertWeatherBearing

--- src/main/java/Jawyuhz/DiscordBot/commands/Weather.java
+++ src/main/java/Jawyuhz/DiscordBot/commands/Weather.java
@@ -6,6 +6,7 @@ import com.github.dvdme.ForecastIOLib.FIODataPoint;
 import com.github.dvdme.ForecastIOLib.ForecastIO;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
+
 import static com.github.dvdme.ForecastIOLib.ForecastIO.UNITS_US;
 
 /**
@@ -30,11 +31,18 @@ public class Weather implements Command {
         fio.getForecast("28.406339", "-81.447637");
         FIOCurrently currently = new FIOCurrently(fio);
         String [] fieldsArray = currently.get().getFieldsArray();
-        e.getTextChannel().sendMessage(parseFieldsArray(fieldsArray, currently)).queue();
-//        for(int i=0; i<fieldsArray.length; i++) {
+        String parsed = parseFieldsArray(fieldsArray, currently);
+        e.getTextChannel().sendMessage(parsed).queue();
+
+
+        //Data Barfer
+        for(int i=0; i<fieldsArray.length; i++) {
+            System.out.println(fieldsArray[i] + ": "+ currently.get().getByKey(fieldsArray[i]));
 //            e.getTextChannel().sendMessage(fieldsArray[i] + ": " + currently.get().getByKey(fieldsArray[i])).queue();
-//            Thread.sleep(500);
-//        }
+        }
+
+
+
     }
     public void executed(boolean success, MessageReceivedEvent event) {
 
@@ -46,85 +54,74 @@ public class Weather implements Command {
         FIODataPoint data;
         String label;
         String curr;
-        if(fieldsArray.length==17) {
 
-            label = fieldsArray[0];
-            curr = currently.get().getByKey(label);
-            parsedString = parsedString.concat(curr + " skies" + "\n");
+        curr = currently.get().getByKey("summary");
+        parsedString = parsedString.concat(curr + "\n");
 
-            label = fieldsArray[1];
-            curr = currently.get().getByKey(label);
-            parsedString = parsedString.concat("Probability of Precipitation: " + curr + "% \n");
-//            Cannot implement precipitation intensity: Units unknown
+        //precip Probability
+        curr = currently.get().getByKey("precipProbability");
+        parsedString = parsedString.concat("Probability of Precipitation: " + curr + "% \n");
+//        Cannot implement precipitation intensity: Units unknown
 //            if(Integer.parseInt(curr)!=0) {
 //                label = fieldsArray[3];
 //                curr = currently.get().getByKey(label);
 //                parsedString = "    Intensity: " + curr +
 //            }
 
-
-            label = fieldsArray[2];
-            curr = currently.get().getByKey(label);
-            parsedString = parsedString.concat("Current Visibility: " + curr + " mi\n");
-
-            label = fieldsArray[5];
-            curr = currently.get().getByKey(label);
-            parsedString = parsedString.concat("Cloud Cover: " + curr + " oktas\n");
-
-            label = fieldsArray[16];
-            curr = currently.get().getByKey(label);
-            parsedString = parsedString.concat("Wind Speed: " + curr + " mph\n");
+        //Visibility
+        curr = currently.get().getByKey("visibility");
+        parsedString = parsedString.concat("Current Visibility: " + curr + " mi\n");
+//
+        //Cloud Cover
+        curr = currently.get().getByKey("cloudCover");
+        parsedString = parsedString.concat("Cloud Cover: " + curr + " oktas\n");
+//
+        //Wind Speed
+        curr = currently.get().getByKey("windSpeed");
+        parsedString = parsedString.concat("Wind Speed: " + curr + " mph\n");
 
 //          Wind bearing needs a conversion factor into a cardinal direction
 //            label = fieldsArray[6];
 //            curr = currently.get().getByKey(label);
 //            parsedString = parsedString.concat("Windbearing: " + curr + " \n");
+//
+        //Temperature
+        curr = currently.get().getByKey("temperature");
+        parsedString = parsedString.concat("Temperature: " + curr + " degrees\n");
 
-            label = fieldsArray[13];
-            curr = currently.get().getByKey(label);
-            parsedString = parsedString.concat("Temperature: " + curr + " degrees\n");
+        //Apparent Temperature
+        curr = currently.get().getByKey("apparentTemperature");
+        parsedString = parsedString.concat("Apparent Temperature: " + curr + " degrees\n");
 
-            label = fieldsArray[7];
-            curr = currently.get().getByKey(label);
-            parsedString = parsedString.concat("Apparent Temperature: " + curr + " degrees\n");
+        //Pressure
+        curr = currently.get().getByKey("pressure");
+        parsedString = parsedString.concat("Pressure: " + curr + " mb\n");
 
-            label = fieldsArray[8];
-            curr = currently.get().getByKey(label);
-            parsedString = parsedString.concat("Pressure: " + curr + " mb\n");
+        //Humidity
+        curr = currently.get().getByKey("humidity");
+        double mid = Double.parseDouble(curr)*100;
+        curr = Double.toString(mid);
+        curr = (curr.replaceAll("0\\.",""));
+        parsedString = parsedString.concat("Humidity: " + curr + "%\n");
 
-            label = fieldsArray[14];
-            curr = currently.get().getByKey(label);
-            parsedString = parsedString.concat("Humidity: " + curr + "%\n");
-
-            label = fieldsArray[9];
-            curr = currently.get().getByKey(label);
-            parsedString = parsedString.concat("Dew Point: " + curr + " degrees\n");
+        //Dew Point
+        curr = currently.get().getByKey("dewPoint");
+        parsedString = parsedString.concat("Dew Point: " + curr + " degrees\n");
 
 //            needs units before implementation
 //            label = fieldsArray[11];
 //            curr = currently.get().getByKey(label);
 //            parsedString = parsedString.concat("Nearest Storm Bearing: " + curr + "\n");
 
-            label = fieldsArray[12];
-            curr = currently.get().getByKey(label);
-            parsedString = parsedString.concat("Nearest Storm: " + curr + " mi \n");
+        //Nearest Storm
+        curr = currently.get().getByKey("nearestStormDistance");
+        parsedString = parsedString.concat("Nearest Storm: " + curr + " mi \n");
 
+        //UV Index
+        curr = currently.get().getByKey("uvIndex");
+        parsedString = parsedString.concat("UV Index: "+ curr);
 
-
-
-
-
-
-            return parsedString;
-
-
-
-        }
-
-
-
-
-        return null;
+        return parsedString;
     }
 //  http://climate.umn.edu/snow_fence/components/winddirectionanddegreeswithouttable3.htm
 //    private int convertBearing(String bearing) {
